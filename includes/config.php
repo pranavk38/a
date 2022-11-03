366267a7c8a7574a7938871c7d839bfbfc011bc9
pranavk38 <114636301+pranavk38@users.noreply.github.com>
2022-11-03 23:39:05 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website',
+        'name' => 'Plants',
         'site_url' => '',
         'pretty_uri' => false,
         'nav_menu' => [
